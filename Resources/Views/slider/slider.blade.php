7901af2c4e3d3b26a0b3f5803a6394f164462bbe
SherifAshraf <sherif.marvel@outlook.com>
2015-06-14 17:01:55 +0200

    Changes

--- Resources/Views/slider/slider.blade.php
+++ Resources/Views/slider/slider.blade.php
@@ -15,30 +15,30 @@
       </thead>
       <tbody>
         @foreach($sliders as $slider)
-        <tr>
-          <th>{{ $slider->id }}</th>
-          <th>{{ $slider->title }}</th>
-          <th>{{ $slider->description }}</th>
-          <th>
-             @if(\CMS::permissions()->can('change-status', 'Sliders'))
-                    @if($slider->is_active == 0)
-                      <a class="btn btn-default" href='{{ url("admin/slider/activate/$slider->id") }}' role="button">Activate</a>
-                    @else
-                      <a class="btn btn-default active" href='{{ url("admin/slider/deactivate/$slider->id") }}'  role="button">Deactivate</a>
+          <tr>
+            <th>{{ $slider->id }}</th>
+            <th>{{ $slider->title }}</th>
+            <th>{{ $slider->description }}</th>
+            <th>
+               @if(\CMS::permissions()->can('change-status', 'Sliders'))
+                      @if($slider->is_active == 0)
+                        <a class="btn btn-default" href='{{ url("admin/slider/activate/$slider->id") }}' role="button">Activate</a>
+                      @else
+                        <a class="btn btn-default active" href='{{ url("admin/slider/deactivate/$slider->id") }}'  role="button">Deactivate</a>
+                      @endif
                     @endif
-                  @endif
-              @if(\CMS::permissions()->can('show', 'Sliders'))
-              <a 
-              class ="btn btn-default" 
-              href  ='{{ url("admin/slider/slideshow/show", $slider->slider_slug) }}' 
-              role  ="button">
-              Slide shows
-              </a> 
-            @endif
-          </th>
-        </tr>
+                @if(\CMS::permissions()->can('show', 'Sliders'))
+                <a 
+                class ="btn btn-default" 
+                href  ='{{ url("admin/slider/slideshow/show", $slider->slider_slug) }}' 
+                role  ="button">
+                Slide shows
+                </a> 
+              @endif
+            </th>
+          </tr>
+        @endforeach
       </tbody>
-      @endforeach
     </table>
   </div>
 </div>
--- Resources/Views/slideshow/slideshow.blade.php
+++ Resources/Views/slideshow/slideshow.blade.php
@@ -20,39 +20,39 @@
       </thead>
       <tbody>
         @foreach($slideShows as $slide)
-        <tr>
-          <th>{{ $slide->id }}</th>
-          <th>{{ $sliderSlug }}</th>
-          <th>{!! $slide->description !!}</th>
-          <th>
-          @if(\CMS::permissions()->can('edit', 'SlideShows'))
-                <a class="btn btn-default" href='{{ url("admin/slider/slideshow/edit/$slide->id", $sliderSlug) }}' role="button">Edit</a>
-          @endif
-
-          @if(\CMS::permissions()->can('delete', 'SlideShows'))
-                <a class="btn btn-default" href='{{ url("admin/slider/slideshow/delete/$slide->id") }}' role="button">Delete</a>
-          @endif
-
-          @if(\CMS::permissions()->can('edit', 'SlideShows'))
-            @if($slide->status == 'disabled')
-                  <a class="btn btn-default" href='{{ url("admin/slider/slideshow/enable/$slide->id") }}' role="button">Enable</a>
-            @else
-                  <a class="btn btn-default active" href='{{ url("admin/slider/slideshow/disable/$slide->id") }}' role="button">Disable</a>
+          <tr>
+            <th>{{ $slide->id }}</th>
+            <th>{{ $sliderSlug }}</th>
+            <th>{!! $slide->description !!}</th>
+            <th>
+            @if(\CMS::permissions()->can('edit', 'SlideShows'))
+                  <a class="btn btn-default" href='{{ url("admin/slider/slideshow/edit/$slide->id", $sliderSlug) }}' role="button">Edit</a>
             @endif
-          @endif
 
-          @if(\CMS::permissions()->can('show', 'LanguageContents'))
-            <a 
-            class ="btn btn-default" 
-            href  ='{{ url("admin/language/languagecontents/show/slide_show/$slide->id") }}'
-            role  ="button">
-            Translations
-            </a> 
-          @endif
-          </th>
-        </tr>
+            @if(\CMS::permissions()->can('delete', 'SlideShows'))
+                  <a class="btn btn-default" href='{{ url("admin/slider/slideshow/delete/$slide->id") }}' role="button">Delete</a>
+            @endif
+
+            @if(\CMS::permissions()->can('edit', 'SlideShows'))
+              @if($slide->status == 'disabled')
+                    <a class="btn btn-default" href='{{ url("admin/slider/slideshow/enable/$slide->id") }}' role="button">Enable</a>
+              @else
+                    <a class="btn btn-default active" href='{{ url("admin/slider/slideshow/disable/$slide->id") }}' role="button">Disable</a>
+              @endif
+            @endif
+
+            @if(\CMS::permissions()->can('show', 'LanguageContents'))
+              <a 
+              class ="btn btn-default" 
+              href  ='{{ url("admin/language/languagecontents/show/slide_show/$slide->id") }}'
+              role  ="button">
+              Translations
+              </a> 
+            @endif
+            </th>
+          </tr>
+        @endforeach
       </tbody>
-      @endforeach
     </table>
   </div>
 </div>
